36ac4f98dc9c12fede26bce60b16435ee54df212
Nicolas Pinto <pinto@mit.edu>
2011-02-24 14:09:33 -0500

    add terminator configuration

---
+++ terminator/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+backup_dir=backup-$(date +"%Y-%m-%d_%Hh%Mm%Ss")
+
+function verbose {
+    echo $*
+    $*
+}
+
+for f in .config/terminator; do
+    echo $f;
+    if [[ -e ~/$f ]]; then
+        verbose mkdir -p $backup_dir;
+        verbose mv ~/$f $backup_dir;
+    fi;
+    verbose ln -sf $(pwd)/$f ~/$f;
+done;
+
+# vim: expandtab tabstop=4 shiftwidth=4 autoindent smartindent tw=80:
