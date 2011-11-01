d8ec5acc2f3bfcbe9a71e5dbfaae8f8c0097ee9b
Nicolas Pinto <pinto@mit.edu>
2011-11-01 17:05:55 -0400

    Add ssh and aws config, Dropbox-enabled

---
+++ ssh/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+backup_dir=backup-$(date +"%Y-%m-%d_%Hh%Mm%Ss")
+
+set -x
+set -e
+
+srcdn=$HOME/Dropbox/config/ssh
+dstdn=$HOME/.ssh
+for srcfn in $srcdn/{config,.sh.d}; do
+    echo $srcfn;
+    srcbn=$(basename $srcfn);
+    dstfn=$dstdn/$srcbn
+    if [[ -e $dstfn ]]; then
+        mkdir -p $backup_dir;
+        mv $dstfn $backup_dir;
+    fi;
+    ln -sf $srcfn $dstfn;
+done;
+
+# vim: expandtab tabstop=4 shiftwidth=4 autoindent smartindent tw=80:
