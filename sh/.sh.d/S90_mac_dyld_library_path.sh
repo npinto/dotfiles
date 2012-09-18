c299f6a56ffc768dbe5d5a144f10897329bab13b
Nicolas Pinto <pinto@alum.mit.edu>
2012-09-17 22:42:45 -0700

    BUG: fix 10.8 osx stuff

---
+++ sh/.sh.d/S90_mac_dyld_library_path.sh
@@ -0,0 +1,10 @@
+if [[ "`uname`" == "Darwin" ]]; then
+    if [[ "`uname -r`" == "10.8.0" ]]; then
+        export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
+    fi;
+    if [[ "`uname -r`" == "12.1.0" ]]; then
+    fi;
+fi;
+
+
+
