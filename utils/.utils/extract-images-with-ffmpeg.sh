858b17831d470da3b8bd4f307e3ec201cc9f2dc4
Nicolas Pinto <pinto@alum.mit.edu>
2012-10-23 15:43:49 -0700

    FIX: framerate workaround

--- utils/.utils/extract-images-with-ffmpeg.sh
+++ utils/.utils/extract-images-with-ffmpeg.sh
@@ -4,8 +4,10 @@ in_fn="$1"
 ext=${2:-jpg}
 fps=${3:-30}
 out_dir=${4:-"${in_fn}.${ext}_files"}
+framerate=$([[ ${fps} -ne 0 ]] && echo "-r ${fps}")
+
 echo "Using ${in_fn}"
-echo "Using ${fps} fps"
+echo "Using ${fps} fps: option='${framerate}'"
 
 set -e
 set -x
@@ -13,5 +15,5 @@ set -x
 test ! -d "${out_dir}"
 
 mkdir -p "${out_dir}"
-ffmpeg -i "${in_fn}" -an -f image2 -q:v 1 -r ${fps} -vsync 0 "${out_dir}/%09d.${ext}"
+echo ffmpeg -i "${in_fn}" -an -f image2 -q:v 1 ${framerate} -vsync 0 "${out_dir}/%09d.${ext}"
 # -vsync 0 will not dump dupes
