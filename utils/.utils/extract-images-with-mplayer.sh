25024745ad16dc735c777d670e9562005107029a
Nicolas Pinto <pinto@alum.mit.edu>
2012-11-01 02:51:24 -0700

    ENH: add extract script with mplayer

---
+++ utils/.utils/extract-images-with-mplayer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+in_fn="$1"
+ext=${2:-jpeg}
+fps=${3:-30}
+out_dir=${4:-"${in_fn}.${ext}_files"}
+framerate=$([[ ${fps} -ne 0 ]] && echo "-r ${fps}")
+
+echo "Using ${in_fn}"
+echo "Using ${fps} fps: option='${framerate}'"
+
+command -v mplayer
+
+set -e
+set -x
+
+test ! -d "${out_dir}"
+
+# the following doesn't work with png
+#mplayer "${in_fn}" -vo ${ext}:quality=100:outdir="${out_dir}"
+mplayer "${in_fn}" -vo ${ext}:outdir="${out_dir}"
