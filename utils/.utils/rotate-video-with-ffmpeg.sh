05bafddc636c6326180dde49694ae4c8b6e2eaaa
Nicolas Pinto <pinto@alum.mit.edu>
2013-01-17 16:15:28 -0800

    ENH: ffmpeg rotate script now runnable in the background

--- utils/.utils/rotate-video-with-ffmpeg.sh
+++ utils/.utils/rotate-video-with-ffmpeg.sh
@@ -9,4 +9,5 @@ out_fn=${4:-"${in_fn}.transpose=${trans}.${in_ext}"}
 set -e
 set -x
 
-ffmpeg -i "${in_fn}" -vf transpose=${trans} -an -q:v 1 "${out_fn}"
+#ffmpeg -y -loglevel 0 -i "${in_fn}" -vf transpose=${trans} -an -q:v 1 "${out_fn}" < /dev/null
+ffmpeg -y -i "${in_fn}" -vf transpose=${trans} -an -q:v 1 "${out_fn}" < /dev/null
