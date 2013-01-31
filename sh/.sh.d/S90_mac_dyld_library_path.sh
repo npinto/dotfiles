7fc623d99ae7b08a1b90190fabbf4ad020e28d94
Nicolas Pinto <pinto@alum.mit.edu>
2013-01-30 22:12:52 -0800

    FIX: dyld mac stuff

--- sh/.sh.d/S90_mac_dyld_library_path.sh
+++ sh/.sh.d/S90_mac_dyld_library_path.sh
@@ -1,7 +1,7 @@
-if [[ "`uname`" == "Darwin" ]]; then
-    if [[ "`uname -r`" == "10.8.0" ]]; then
-        export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
-    fi;
-    #if [[ "`uname -r`" == "12.1.0" ]]; then
+#if [[ "`uname`" == "Darwin" ]]; then
+    #if [[ "`uname -r`" == "10.8.0" ]]; then
+        #export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
     #fi;
-fi;
+    ##if [[ "`uname -r`" == "12.1.0" ]]; then
+    ##fi;
+#fi;
