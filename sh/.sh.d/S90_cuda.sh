b2b59e0bf20d268a20f706265cf998565a9bd5c7
Nicolas Pinto <pinto@alum.mit.edu>
2013-06-14 17:31:23 -0700

    FIX: add cuda sh.d for lib64 in gentoo

--- sh/.sh.d/S90_cuda.sh
+++ sh/.sh.d/S90_cuda.sh
@@ -3,7 +3,11 @@
 # -----------------------------------------------------------------------------
 
 if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.1.0" ]]; then
+    # -- Debian/Ubuntu
     test -d /usr/local/cuda/bin && export PATH=$PATH:/usr/local/cuda/bin;
     test -d /usr/local/cuda/lib && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib;
     test -d /usr/local/cuda/lib64 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64;
+    # -- Gentoo
+    test -d /opt/cuda/lib64 && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/cuda/lib64
 fi;
+
