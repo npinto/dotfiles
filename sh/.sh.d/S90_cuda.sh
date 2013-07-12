8ecd43060e25cbe1d3fa77184ec7d03677adaf3b
Nicolas Pinto <pinto@alum.mit.edu>
2013-07-11 18:46:56 -0700

    FIX: various

--- sh/.sh.d/S90_cuda.sh
+++ sh/.sh.d/S90_cuda.sh
@@ -2,7 +2,7 @@
 # -- CUDA related
 # -----------------------------------------------------------------------------
 
-if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.1.0" ]]; then
+if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.4.0" ]]; then
     test -d /usr/local/cuda/bin && export PATH=$PATH:/usr/local/cuda/bin;
     test -d /usr/local/cuda/lib && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib;
     test -d /usr/local/cuda/lib64 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64;
