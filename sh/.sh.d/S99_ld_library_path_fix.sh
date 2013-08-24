6ddd36e206aa7b365f339ee45404ddaa7965b807
Nicolas Pinto <pinto@alum.mit.edu>
2013-08-23 18:07:30 -0700

    na

--- sh/.sh.d/S99_ld_library_path_fix.sh
+++ sh/.sh.d/S99_ld_library_path_fix.sh
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 # remove useless semicolons from LD_LIBRARY_PATH
 
-if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.1.0" ]]; then
+if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.4.0" ]]; then
     export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed -e 's/:*$//g' -e 's/^:*//g')
 fi;
 
