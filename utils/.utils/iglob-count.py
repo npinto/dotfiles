42dcf64bd70235880f717d0927b107ce32cd9042
Nicolas Pinto <pinto@alum.mit.edu>
2014-06-12 10:48:32 -0700

    ENH: add youtube-dl-srt and iglob-count

---
+++ utils/.utils/iglob-count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+from glob import iglob
+pattern = sys.argv[1]
+print pattern
+fnames = iglob(pattern)
+i = 0
+for fname in fnames:
+    i += 1
+    if i % 100 == 0:
+        print '%e' % i
+
