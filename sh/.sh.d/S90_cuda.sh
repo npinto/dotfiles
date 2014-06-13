9e95476c33c101841887c47f72f2deca524b7c91
Nicolas Pinto <pinto@alum.mit.edu>
2014-06-13 15:03:30 -0700

    na

--- sh/.sh.d/S90_cuda.sh
+++ sh/.sh.d/S90_cuda.sh
@@ -3,12 +3,12 @@
 # -----------------------------------------------------------------------------
 
 if ! [[ "`uname`" == "Darwin" && "`uname -r`" == "12.4.0" ]]; then
-    test -d /Developer/NVIDIA/CUDA-6.0/bin && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/bin/
-    test -d /Developer/NVIDIA/CUDA-6.0/lib && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/lib/
-    test -d /Developer/NVIDIA/CUDA-6.0/lib64 && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/lib64/
-    #test -d /usr/local/cuda/bin && export PATH=$PATH:/usr/local/cuda/bin;
-    #test -d /usr/local/cuda/lib && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib;
-    #test -d /usr/local/cuda/lib64 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64;
+    #test -d /Developer/NVIDIA/CUDA-6.0/bin && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/bin/
+    #test -d /Developer/NVIDIA/CUDA-6.0/lib && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/lib/
+    #test -d /Developer/NVIDIA/CUDA-6.0/lib64 && export PATH=$PATH:/Developer/NVIDIA/CUDA-6.0/lib64/
+    test -d /usr/local/cuda/bin && export PATH=$PATH:/usr/local/cuda/bin;
+    test -d /usr/local/cuda/lib && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib;
+    test -d /usr/local/cuda/lib64 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64;
     # -- Gentoo
     test -d /opt/cuda/lib64 && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/cuda/lib64
 fi;
