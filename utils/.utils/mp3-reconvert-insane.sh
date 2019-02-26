47ccd9b260217d17a047b53e3581cf860e5ee24b
Nicolas Pinto <pinto@alum.mit.edu>
2019-02-26 00:19:04 -0800

    na

---
+++ utils/.utils/mp3-reconvert-insane.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+TMPDIR=$(mktemp -d)
+WAV="${TMPDIR}/$(basename "$1")".wav
+OUT="$1".2.mp3
+
+ffmpeg -i "$1" "${WAV}"
+lame --preset insane "${WAV}" "${OUT}"
+
+rm -rvf ${TMPDIR}
