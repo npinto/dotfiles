cd98d8bf473aeeb9304f218dc0047b69459e36b5
Nicolas Pinto <pinto@alum.mit.edu>
2019-04-13 14:41:13 -0700

    na

--- utils/.utils/mp3-id3-cover-only.sh
+++ utils/.utils/mp3-id3-cover-only.sh
@@ -6,11 +6,14 @@ set -x
 TMPDIR=$(mktemp -d)
 TMPWAV=${TMPDIR}/$(basename "$1").wav
 OUTMP3="$1".PBQA_WO.mp3
+LOG=${TMPDIR}/log.txt
 
-eyeD3 --write-images=${TMPDIR} "$1"
+eyeD3 --no-color --write-images=${TMPDIR} "$1" | tee ${LOG}
+grep Writing ${LOG}
 ffmpeg -i "$1" "${TMPWAV}"
 lame --preset insane "${TMPWAV}" "${OUTMP3}"
-FRONT_COVER=$(ls "${TMPDIR}"/FRONT_COVER*)
+#FRONT_COVER=$(ls "${TMPDIR}"/FRONT_COVER*)
+FRONT_COVER=$(grep Writing ${LOG} | cut -d' ' -f2 | sed 's/\.\.\.$//g' | head -n1)
 eyeD3 --add-image "${FRONT_COVER}:FRONT_COVER" "${OUTMP3}"
 
 rm -rvf ${TMPDIR}
