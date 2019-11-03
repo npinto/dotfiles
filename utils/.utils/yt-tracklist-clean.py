7a483054e7d09e84fcf24db8b0674670a5a76016
Nicolas Pinto <pinto@alum.mit.edu>
2019-11-03 17:16:04 +0900

    na

---
+++ utils/.utils/yt-tracklist-clean.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+
+infn = sys.argv[1]
+
+data = open(infn).readlines()
+
+grep_v_in_l = [
+    'Copyright owners',
+    'Ad revenue',
+    'Content found during',
+    'some territories',
+    'cannot be monetized',
+    'Copyright owner',
+    'On behalf',
+    'GmbH',
+    'LLC',
+    'Content used',
+    'Claim type',
+    'Impact on the video',
+    'Actions',
+    '/',
+]
+grep_v_eq_l = [
+    '1.00\n'
+]
+
+out_lines = []
+accum = []
+for line in data:
+    skip = False
+    for grep in grep_v_in_l:
+        if grep in line:
+            skip = True
+            break
+    if skip:
+        continue
+    for grep in grep_v_eq_l:
+        if line == grep:
+            skip = True
+            break
+    if skip:
+        continue
+    line = line.replace(' – ', ' => ')
+    accum.append(line)
+    if ' => ' in line:
+        if len(accum) == 3:
+            out_line = accum[1].strip() + ' - ' + accum[0].strip()
+        elif len(accum) == 4:
+            out_line = accum[2].strip() + ' - ' + accum[1].strip()
+        else:
+            raise IndexError
+        out_lines.append(out_line + '\n')
+        out_lines.append(accum[-1] + '\n')
+        accum = []
+
+print(''.join(out_lines))
