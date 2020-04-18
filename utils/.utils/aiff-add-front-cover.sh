81a855f00fbf6c47527644a36f2f596ac70931dd
Nicolas Pinto <pinto@alum.mit.edu>
2020-04-17 18:36:02 -0700

    na

--- utils/.utils/aiff-add-front-cover.sh
+++ utils/.utils/aiff-add-front-cover.sh
@@ -3,4 +3,4 @@
 frontcover="$1"
 audiofile="$2"
 
-kid3-cli -c 'select "${audiofile}"' -c 'set picture:"${frontcover}" "Picture Description"' -c 'save'
+kid3-cli -c 'select "'"${audiofile}"'"' -c 'set picture:"'"${frontcover}"'" "Picture Description"' -c 'save'
--- utils/.utils/bin-to-iso.sh
+++ utils/.utils/bin-to-iso.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-echo bchunk file.bin file.cue file.iso
+echo "bchunk file.bin file.cue file.iso [-w for wav]"
 
-bchunk "$1" "$2" "$3"
+bchunk "$4" "$1" "$2" "$3"
