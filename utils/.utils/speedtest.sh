9487ae48f0abd194da1bd96d19ddb87e2d531756
Nicolas Pinto <nicolas.pinto@gmail.com>
2025-09-22 09:05:19 -0700

    make speedtest secure

--- utils/.utils/speedtest.sh
+++ utils/.utils/speedtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - --secure
