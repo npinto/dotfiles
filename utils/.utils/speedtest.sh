01319a1ad551a9577ca59a73c80c3aa3b5e9762b
Nicolas Pinto <nicolas.pinto@gmail.com>
2025-10-22 09:36:18 -0700

    Add more stuff

--- utils/.utils/speedtest.sh
+++ utils/.utils/speedtest.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+set -e
+set -x
+
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - --secure
---
+++ utils/.utils/whatismyip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl ipinfo.io/json
